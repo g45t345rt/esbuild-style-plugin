080f0faba50e47983d09d7c5893ed40f3d1559d5
Artem Titoulenko <artem.titoulenko@gmail.com>
2023-01-26 22:56:29 +0000

    Add test for less modules

--- test/css_modules/src/index.ts
+++ test/css_modules/src/index.ts
@@ -1,4 +1,5 @@
 import cssStyles from './styles.module.css'
 import sassStyles from './styles.module.sass'
+import lessStyles from './styles.module.less'
 
-console.log(cssStyles, sassStyles)
+console.log(cssStyles, sassStyles, lessStyles)
